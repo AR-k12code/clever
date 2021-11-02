50b0729db716a4c9c124ec1232a68cc6a7fddacb
Craig Millsap <craig@carbm1.com>
2021-11-02 10:35:58 -0500

    Dedupe Sections and Contact_sis_id

--- clever.ps1
+++ clever.ps1
@@ -24,7 +24,7 @@ project and would need to be run prior to this script.
 
 #>
 
-Start-Transcript "$PSScriptRoot\clever-log.log" -Force
+try { Start-Transcript "$PSScriptRoot\clever-log.log" -Force } catch {}
 $cleverhostkey = '76:0c:bb:e5:f7:df:97:c3:f2:77:0d:9a:2e:d7:92:18'
 
 #Check for SimplySQL Module
@@ -65,39 +65,8 @@ $reports = @{
     'enrollments' = @{ 'parameters' = ''; 'reportname' = 'enrollments_markingperiod' }
     'schools' = @{ 'parameters' = ''; 'reportname' = 'schools' }
     'sections' = @{ 'parameters' = "p_year=$schoolyear"; 'reportname' = 'sections' }
-    'students' = @{ 'parameters' = ''; 'reportname' = 'students' }
+    'students' = @{ 'parameters' = ''; 'reportname' = 'students_v2' }
     'teachers' = @{ 'parameters' = ''; 'reportname' = 'teachers' }
-}
-
-if ($current_term_only -eq $True) {
-    Write-Host "Info: Current term only specified. This requires the sections.csv file to find the current date."
-    
-    if (Test-Path $PSScriptRoot\downloads\sections.csv) {
-
-        #Open sections.csv and find the current term.
-        $sections = Import-Csv $PSScriptRoot\downloads\sections.csv
-        $term1 = $sections | Where-Object { $PSItem.Term_name -eq 1 } | Select-Object -Property Term_name,Term_start,Term_end -First 1
-        $term2 = $sections | Where-Object { $PSItem.Term_name -eq 2 } | Select-Object -Property Term_name,Term_start,Term_end -First 1
-        $term3 = $sections | Where-Object { $PSItem.Term_name -eq 3 } | Select-Object -Property Term_name,Term_start,Term_end -First 1
-        $term4 = $sections | Where-Object { $PSItem.Term_name -eq 4 } | Select-Object -Property Term_name,Term_start,Term_end -First 1
-    
-        $today = Get-Date
-        if (([Int]$(Get-Date -Format MM) -ge 7) -And ($today -le [datetime]$term1.Term_end)) {
-            $currentTerm = 1
-        } elseif (($today -gt [datetime]$term1.Term_end) -And ($today -le [datetime]$term2.Term_end)) {
-            $currentTerm = 2
-        } elseif (($today -gt [datetime]$term2.Term_end) -And ($today -le [datetime]$term3.Term_end)) {
-            $currentTerm = 3
-        } else {
-            $currentTerm = 4 #why do math when its the only option left?
-        }
-
-        Write-Host "Info: Pulling term $currentTerm for enrollments.csv"
-        $reports.'enrollments' = @{ 'parameters' = "p_mp=$currentTerm"; 'reportname' = 'enrollments' }
-    } else {
-        $sections_error = $True
-        Write-Host "Error: sections.csv not found. This could be because this is the first time you have ran this script. You will need to run the script again." -ForegroundColor Red
-    }
 }
 
 #Establish Session Only. Report parameter is required but we can provide a fake one for authentication only.
@@ -133,25 +102,10 @@ if (($failedJobs | Measure-Object).count -ge 1) {
     exit(2)
 }
 
-
-if ($sections_error) {
-    #we need to verify sections.csv file now exists and run again.
-    if (Test-Path $PSScriptRoot\downloads\sections.csv) {
-        Write-Host "Info: You must run this script again in order to pull the current term only for enrollments."
-        exit(0)
-    } else {
-        Write-Host "Error: You specified you want the current term only but you're missing the sections.csv file and it failed to download." -ForegroundColor Red
-        exit(1)
-    }
-}
-
 #If full schedule then we need to build the sql tables and match enrollment to sections.
-if ($current_term_only) {
-    Copy-Item $PSScriptRoot\downloads\enrollments.csv $PSScriptRoot\files\enrollments.csv -Force
-} else {
-    $database = "$PSScriptRoot\database.sqlite"
+$database = "$PSScriptRoot\database.sqlite"
 
-    $sql_import = @"
+$sql_import = @"
 drop table if exists enrollments_csv_import;
 .mode csv
 .separator ,
@@ -162,11 +116,11 @@ drop table if exists sections_csv_import;
 .import downloads\\sections.csv sections_csv_import
 "@
 
-    $sql_import | & $PSScriptRoot\bin\sqlite3.exe $database
+$sql_import | & $PSScriptRoot\bin\sqlite3.exe $database
 
-    Open-SQLiteConnection -DataSource $database
+Open-SQLiteConnection -DataSource $database
 
-    Invoke-SqlUpdate -Query '/* FINAL REVISION! */
+Invoke-SqlUpdate -Query '/* FINAL REVISION! */
     /* To ensure the table columns are correct I am dropping the entire table instead of truncating. */
     DROP TABLE IF EXISTS `enrollments_grouped`;
     CREATE TABLE `enrollments_grouped` (
@@ -177,18 +131,18 @@ drop table if exists sections_csv_import;
       UNIQUE (`School_id`,`Section_id`,`Student_id`,`Terms`)
     );' | Out-Null
 
-    Start-SqlTransaction
+Start-SqlTransaction
 
-    Invoke-SqlUpdate -Query '/* I need the terms to be grouped together to query later. Make a copy of the enrollments table with all terms grouped together. */
+Invoke-SqlUpdate -Query '/* I need the terms to be grouped together to query later. Make a copy of the enrollments table with all terms grouped together. */
     REPLACE INTO `enrollments_grouped`
       SELECT School_id,Section_id,Student_id,group_concat(Marking_period,'''') as Terms
       FROM enrollments_csv_import
       GROUP BY Student_id,Section_id
       ORDER BY Marking_period;' | Out-Null
 
-    Complete-SqlTransaction
+Complete-SqlTransaction
 
-    Invoke-SqlUpdate -Query '/* I need the terms to be grouped together to query later. Make a copy of the sections table with all terms grouped together. */
+Invoke-SqlUpdate -Query '/* I need the terms to be grouped together to query later. Make a copy of the sections table with all terms grouped together. */
     DROP TABLE IF EXISTS `sections_grouped`;
     CREATE TABLE `sections_grouped` (
       `Terms` int(4),
@@ -210,15 +164,15 @@ drop table if exists sections_csv_import;
 
     Start-SqlTransaction
 
-    Invoke-SqlUpdate -Query 'REPLACE INTO sections_grouped
+Invoke-SqlUpdate -Query 'REPLACE INTO sections_grouped
       SELECT group_concat(Term_name,'''') as `Terms`,`School_id`, `Section_id`, `Teacher_id`, `Name`, `Section_number`, `Grade`, `Course_name`, `Course_number`, `Course_description`, `Period`, `Subject`, `Term_start`, `Term_end`
       FROM `sections_csv_import`
       GROUP BY `Section_id`
       ORDER BY `Terms`' | Out-Null
 
-    Complete-SqlTransaction
+Complete-SqlTransaction
 
-    Invoke-SqlUpdate -Query 'DROP TABLE IF EXISTS `enrollments`;
+Invoke-SqlUpdate -Query 'DROP TABLE IF EXISTS `enrollments`;
     CREATE TABLE `enrollments` (
       `School_id` int(4),
       `Section_id` bigint(32),
@@ -226,60 +180,62 @@ drop table if exists sections_csv_import;
       UNIQUE (`School_id`,`Section_id`,`Student_id`)
     );' | Out-Null
 
-    Start-SqlTransaction
+Start-SqlTransaction
 
-    Invoke-SqlUpdate -Query '/* This requires that teachers have an email address. This is on purpose for my district because of sections that are placeholders. */
+Invoke-SqlUpdate -Query '/* This requires that teachers have an email address. This is on purpose for my district because of sections that are placeholders. */
       INSERT INTO `enrollments`
       SELECT `enrollments_grouped`.`School_id`, `enrollments_grouped`.`Section_id`, `enrollments_grouped`.`Student_id`
       FROM `enrollments_grouped`
       INNER JOIN `sections_grouped` ON `enrollments_grouped`.`Section_id` = `sections_grouped`.`Section_id` AND `enrollments_grouped`.`Terms` = `sections_grouped`.`Terms`
       ORDER BY `enrollments_grouped`.`Student_id`,`sections_grouped`.`Period`;' | Out-Null
     
-    $term1 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 1 LIMIT 1"
-    $term2 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 2 LIMIT 1"
-    $term3 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 3 LIMIT 1"
-    $term4 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 4 LIMIT 1"
-  
-    $today = Get-Date
-    if (([Int]$(Get-Date -Format MM) -gt 7) -And ($today -le [datetime]$term1.Term_end)) {
-        $currentTerm = 1
-    } elseif (($today -gt [datetime]$term1.Term_end) -And ($today -le [datetime]$term2.Term_end)) {
-        $currentTerm = 2
-    } elseif (($today -gt [datetime]$term2.Term_end) -And ($today -le [datetime]$term3.Term_end)) {
-        $currentTerm = 3
-    } else {
-        $currentTerm = 4 #why do math when its the only option left?
-    }
+$term1 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 1 LIMIT 1"
+$term2 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 2 LIMIT 1"
+$term3 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 3 LIMIT 1"
+$term4 = Invoke-SqlQuery -Query "SELECT Term_name,Term_start,Term_end FROM sections_csv_import WHERE Term_name = 4 LIMIT 1"
 
-    Invoke-SqlUpdate -Query 'INSERT OR REPLACE INTO `enrollments`
+$today = Get-Date
+if (([Int]$(Get-Date -Format MM) -gt 7) -And ($today -le [datetime]$term1.Term_end)) {
+    $currentTerm = 1
+} elseif (($today -gt [datetime]$term1.Term_end) -And ($today -le [datetime]$term2.Term_end)) {
+    $currentTerm = 2
+} elseif (($today -gt [datetime]$term2.Term_end) -And ($today -le [datetime]$term3.Term_end)) {
+    $currentTerm = 3
+} else {
+    $currentTerm = 4 #why do math when its the only option left?
+}
+
+Invoke-SqlUpdate -Query 'INSERT OR REPLACE INTO `enrollments`
     SELECT School_id,Section_id,Student_id
     FROM `enrollments_csv_import`
     WHERE Marking_period = ',$currentTerm,'
     ORDER BY Student_id' | Out-Null
 
-    Complete-SqlTransaction
+Complete-SqlTransaction
 
+if ($current_term_only) {
+    Invoke-SqlQuery -Query "SELECT School_id,Section_id,Student_id FROM enrollments_csv_import WHERE Marking_period = $currentTerm" | Export-Csv -UseQuotes AsNeeded -NoTypeInformation -Path "$PSScriptRoot\files\enrollments.csv" -Force
+    Invoke-SqlQuery -Query "SELECT * FROM sections_csv_import WHERE Term_name = $currentTerm" | Export-CSV -UseQuotes AsNeeded -NoTypeInformation -Path "$PSScriptRoot\files\sections.csv" -Force
+} else {
     Invoke-SqlQuery -Query "SELECT School_id,Section_id,Student_id FROM enrollments" | Export-Csv -UseQuotes AsNeeded -NoTypeInformation -Path "$PSScriptRoot\files\enrollments.csv" -Force
 
     #The group by should return the first Term_name in order.
     #pull sections but only the current term or later.
     $sections = Invoke-SqlQuery -Query "/* Pull current term first then terms greater than after*/
-    SELECT * FROM sections_csv_import WHERE Term_name = $currentTerm
-    UNION
-    SELECT * FROM sections_csv_import WHERE Term_name > $currentTerm AND Section_id NOT IN (SELECT Section_id FROM sections_csv_import WHERE Term_name = $currentTerm)
-    GROUP BY Section_id"
+        SELECT * FROM sections_csv_import WHERE Term_name = $currentTerm
+        UNION
+        SELECT * FROM sections_csv_import WHERE Term_name > $currentTerm AND Section_id NOT IN (SELECT Section_id FROM sections_csv_import WHERE Term_name = $currentTerm)
+        GROUP BY Section_id"
 
-    #now I need here term_name -lt $currentTerm but not in $currentTerm and not in > currentTerm.
+        #now I need here term_name -lt $currentTerm but not in $currentTerm and not in > currentTerm.
     $sections += Invoke-SqlQuery -Query "SELECT * FROM sections_csv_import WHERE Term_name < $currentTerm AND Section_id NOT IN (SELECT Section_id FROM sections_csv_import WHERE Term_name >= $currentTerm) GROUP BY Section_id"
 
     $sections | Export-CSV -UseQuotes AsNeeded -NoTypeInformation -Path "$PSScriptRoot\files\sections.csv" -Force
-
 }
 
 Copy-Item $PSScriptRoot\downloads\schools.csv $PSScriptRoot\files\schools.csv -Force
 Copy-Item $PSScriptRoot\downloads\students.csv $PSScriptRoot\files\students.csv -Force
 Copy-Item $PSScriptRoot\downloads\teachers.csv $PSScriptRoot\files\teachers.csv -Force
-#Copy-Item $PSScriptRoot\downloads\sections.csv $PSScriptRoot\files\sections.csv -Force
 
 try {
     Write-Host "Info: Uploading files to Clever..." -ForegroundColor YELLOW
